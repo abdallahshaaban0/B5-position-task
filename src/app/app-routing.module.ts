668ed7bfbc8ef01d42653d266afa135fd9911415
abdallahshaaban <abdallahshaaban135@gmail.com>
2021-04-16 23:25:07 +0200

    force app to redirect to people page as home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { PeopleComponent } from './people/people.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'people',
+  component:PeopleComponent
+},
+{
+  path: '**',
+  redirectTo: 'people'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
